459120dc4895199df266b376ea8d2918a84754e5
ibrahimlawal <ibrahim@paystack.com>
2023-01-19 15:11:34 +0100

    Lint: rewrite stuff to feel better about the code

--- test.js
+++ test.js
@@ -1,58 +1,55 @@
-var nuban = require('./index');
+const nuban = require('./index');
 
-const { validateAccountNumber } = require("./index");
+const { validateAccountNumber } = nuban;
 
+const shouldnotbevalid = validateAccountNumber('0123456789', '011');
+const shouldbevalid = nuban.validate('0123456784', '011');
 
-
-var shouldnotbevalid = validateAccountNumber("0123456789","011");
-var shouldbevalid = nuban.validate("0123456784","011");
-
-
-var checkDigit = null;
-var err1 = null;
+let checkDigit = null;
+let err1 = null;
 try {
-    checkDigit = nuban.calculateCheckDigit("012345678","011");
-} catch(err){
+    checkDigit = nuban.calculateCheckDigit('012345678', '011');
+} catch (err) {
     err1 = err;
 }
 
-var invalidCheckDigit = null;
-var err2 = null;
+const invalidCheckDigit = null;
+let err2 = null;
 try {
-    invalidCheckDigit = nuban.calculateCheckDigit("01234567","011");
-} catch(err){
+    invalidCheckDigit = nuban.calculateCheckDigit('01234567', '011');
+} catch (err) {
     err2 = err;
 }
 
-if(shouldnotbevalid !== false){
-    console.log("Test for invalid nuban failed");
+if (shouldnotbevalid !== false) {
+    console.log('Test for invalid nuban failed');
     process.exit(1);
 }
 
-if(shouldbevalid !== true){
-    console.log("Test for valid nuban failed");
+if (shouldbevalid !== true) {
+    console.log('Test for valid nuban failed');
     process.exit(1);
 }
 
-if(err1 !== null){
-    console.log("Test for valid calculateCheckDigit parameters failed");
+if (err1 !== null) {
+    console.log('Test for valid calculateCheckDigit parameters failed');
     console.error(err1);
     process.exit(1);
 }
 
-if(checkDigit !== 4){
-    console.log("Test for calculateCheckDigit failed");
+if (checkDigit !== 4) {
+    console.log('Test for calculateCheckDigit failed');
     process.exit(1);
 }
 
-if(invalidCheckDigit !== null){
-    console.log("We should not have gotten a response for invalid calculateCheckDigit parameters");
+if (invalidCheckDigit !== null) {
+    console.log('We should not have gotten a response for invalid calculateCheckDigit parameters');
     process.exit(1);
 }
 
-if(err2 === null){
-    console.log("Test for invalid calculateCheckDigit parameters failed");
+if (err2 === null) {
+    console.log('Test for invalid calculateCheckDigit parameters failed');
     process.exit(1);
 }
 
-console.log("ok");
+console.log('ok');
